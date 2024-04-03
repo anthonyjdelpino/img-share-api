d46f0aa4411cab7b1f396202a3a207d73a02fa42
anthonyjdelpino <anthonyjdelpino@gmail.com>
2024-04-03 15:06:31 -0400

    adding test messages and rename package and module

--- go.mod
+++ go.mod
@@ -1,5 +1,19 @@
-module example.com/gcf
+module github.com/anthonyjdelpino/img-share-api
+
+go 1.21.8
+
+require github.com/GoogleCloudPlatform/functions-framework-go v1.8.1
 
 require (
-  github.com/GoogleCloudPlatform/functions-framework-go v1.5.2
+	github.com/cloudevents/sdk-go/v2 v2.14.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
--- index.go
+++ index.go
@@ -1,29 +1,29 @@
-package helloworld
+package imgShareAPI
 
 import (
-  "encoding/json"
-  "fmt"
-  "html"
-  "net/http"
+	"encoding/json"
+	"fmt"
+	"html"
+	"net/http"
 
-  "github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
 
 func init() {
-   functions.HTTP("img-share-api-func", imgShareAPIFunc)
+	functions.HTTP("img-share-api-func", imgShareAPIFunc)
 }
 
 func imgShareAPIFunc(w http.ResponseWriter, r *http.Request) {
-  var d struct {
-    Name string `json:"name"`
-  }
-  if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-    fmt.Fprint(w, "Hello, World!")
-    return
-  }
-  if d.Name == "" {
-    fmt.Fprint(w, "Hello, World!")
-    return
-  }
-  fmt.Fprintf(w, "Hello, %s!", html.EscapeString(d.Name))
+	var d struct {
+		Name string `json:"name"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		fmt.Fprint(w, "Hello, World! Success")
+		return
+	}
+	if d.Name == "" {
+		fmt.Fprint(w, "Hello, World! Success 2")
+		return
+	}
+	fmt.Fprintf(w, "Hello, %s! S", html.EscapeString(d.Name))
 }
