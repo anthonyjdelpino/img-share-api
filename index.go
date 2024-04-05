eb3cbb18d81ce2be5121418799a438b67ae5b758
anthonyjdelpino <anthonyjdelpino@gmail.com>
2024-04-05 15:58:54 -0400

    set up routing for api

--- index.go
+++ index.go
@@ -10,9 +10,31 @@ import (
 func init() {
 	route := gin.Default()
 	route.GET("/", imgShareAPIFunc)
+	route.GET("/images", getAllImages)
+	route.GET("/images/:id", getSpecificImage)
+	route.POST("/images", uploadImage)
+	route.DELETE("images/:id", deleteImage)
+
 	functions.HTTP("imgShareAPIFunc", route.Handler().ServeHTTP)
 }
 
 func imgShareAPIFunc(c *gin.Context) {
+	// provide an api guide here?
 	c.String(http.StatusOK, "img-share-api")
+}
+
+func getAllImages(c *gin.Context) {
+	c.String(http.StatusOK, "list all")
+}
+
+func getSpecificImage(c *gin.Context) {
+	c.String(http.StatusOK, "list specific")
+}
+
+func uploadImage(c *gin.Context) {
+	c.String(http.StatusOK, "upload image")
+}
+
+func deleteImage(c *gin.Context) {
+	c.String(http.StatusOK, "delete image")
 }
