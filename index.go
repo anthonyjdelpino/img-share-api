de710c7fa08674c0d6694c9e17abaa4d298d4081
anthonyjdelpino <anthonyjdelpino@gmail.com>
2024-04-05 15:25:18 -0400

    Switching to use gin webserver

--- index.go
+++ index.go
@@ -1,29 +1,18 @@
 package imgShareAPI
 
 import (
-	"encoding/json"
-	"fmt"
-	"html"
 	"net/http"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/gin-gonic/gin"
 )
 
 func init() {
-	functions.HTTP("imgShareAPIFunc", imgShareAPIFunc)
+	route := gin.Default()
+	route.GET("/", imgShareAPIFunc)
+	functions.HTTP("imgShareAPIFunc", route.Handler().ServeHTTP)
 }
 
-func imgShareAPIFunc(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Name string `json:"name"`
-	}
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello, World! Success")
-		return
-	}
-	if d.Name == "" {
-		fmt.Fprint(w, "Hello, World! Success 2")
-		return
-	}
-	fmt.Fprintf(w, "Hello, %s! Success 3", html.EscapeString(d.Name))
+func imgShareAPIFunc(c *gin.Context) {
+	c.String(http.StatusOK, "img-share-api")
 }
