9a29c98e8a2529c91c97e2e7b8c6d698055403ef
anthonyjdelpino <anthonyjdelpino@gmail.com>
2024-04-05 17:29:20 -0400

    Added GCS storage client

--- index.go
+++ index.go
@@ -1,11 +1,32 @@
 package imgShareAPI
 
+/*TODO:
+implement functions
+structured error messages
+authentication?
+*/
+
 import (
+	"context"
+	"log"
 	"net/http"
 
+	"cloud.google.com/go/storage"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/gin-gonic/gin"
 )
+
+type StorageUser struct {
+	userClient *storage.Client
+	filePath   string
+}
+
+const (
+	projectName = "img-share-api-project"
+	bucketName  = "img-share-api-func-bucket"
+)
+
+var user *StorageUser
 
 func init() {
 	route := gin.Default()
@@ -16,10 +37,22 @@ func init() {
 	route.DELETE("images/:id", deleteImage)
 
 	functions.HTTP("imgShareAPIFunc", route.Handler().ServeHTTP)
+
+	client, err := storage.NewClient(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to create a Google Cloud Storage Client: %v", err)
+	}
+
+	user = &StorageUser{
+		userClient: client,
+		filePath:   "/",
+	}
 }
 
 func imgShareAPIFunc(c *gin.Context) {
 	// provide an api guide here?
+	// 	could use an html thing
+	//	https://gin-gonic.com/docs/examples/html-rendering/
 	c.String(http.StatusOK, "img-share-api")
 }
 
