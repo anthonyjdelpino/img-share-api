31b1aa19c766360f335b56a56140df2bb6c6a20d
anthonyjdelpino <anthonyjdelpino@gmail.com>
2024-04-08 17:10:04 -0400

    Add upload function

--- index.go
+++ index.go
@@ -1,4 +1,5 @@
-package imgShareAPI
+// package imgShareAPI
+package main
 
 /*TODO:
 implement functions
@@ -8,6 +9,7 @@ authentication?
 
 import (
 	"context"
+	"io"
 	"log"
 	"net/http"
 
@@ -15,7 +17,6 @@ import (
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/api/iterator"
-	"google.golang.org/api/option"
 )
 
 type StorageUser struct {
@@ -33,20 +34,22 @@ const (
 var user *StorageUser
 
 func init() {
+	//func main() {
 	route := gin.Default()
 	route.GET("/", imgShareAPIFunc)
 	route.GET("/images", getAllImages)
-	route.GET("/images/:id", getSpecificImage)
+	route.GET("/images/:name", getSpecificImage)
 	route.POST("/images", uploadImage)
 	route.DELETE("/images/:id", deleteImage)
 
 	functions.HTTP("imgShareAPIFunc", route.Handler().ServeHTTP)
 
 	ctx := context.Background()
-	client, err := storage.NewClient(ctx, option.WithoutAuthentication())
+	client, err := storage.NewClient(ctx)
 	if err != nil {
 		log.Fatalf("Failed to create a Google Cloud Storage Client: %v", err)
 	}
+	defer client.Close()
 
 	bucket := client.Bucket(bucketName)
 	user = &StorageUser{
@@ -55,6 +58,8 @@ func init() {
 		bucketHandle: bucket,
 		filePath:     "images/",
 	}
+
+	//route.Run("localhost:8080")
 }
 
 func imgShareAPIFunc(c *gin.Context) {
@@ -84,11 +89,38 @@ func getAllImages(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, imageNames)
 }
 
-func getSpecificImage(c *gin.Context) {
-	c.String(http.StatusOK, "list specific")
+func getSpecificImage(c *gin.Context) { //functions more like a search
+	query := &storage.Query{Prefix: user.filePath + c.Param("name")}
+
+	var imageNames []string
+	it := user.bucketHandle.Objects(*user.context, query)
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate through objects in bucket: %v", err)
+		}
+
+		imageNames = append(imageNames, attrs.Name)
+	}
+
+	c.IndentedJSON(http.StatusOK, imageNames)
 }
 
 func uploadImage(c *gin.Context) {
+	file, err := c.FormFile("file_input")
+	if err != nil {
+		log.Fatalf("Failed to receive file from request.")
+		return
+	}
+	blob, _ := file.Open()
+
+	//add timeout?
+	wc := *user.userClient.Bucket(bucketName).Object(user.filePath + file.Filename).NewWriter(*user.context)
+	io.Copy(&wc, blob)
+	wc.Close()
 	c.String(http.StatusOK, "upload image")
 }
 
