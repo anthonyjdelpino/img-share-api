001bb252888542bab4a20d35cf8fbd2d87e528f9
anthonyjdelpino <anthonyjdelpino@gmail.com>
2024-04-10 18:37:43 -0400

    Add some error checking including file type checks Change output format of getAllImages to use an array of JSON objects

--- index.go
+++ index.go
@@ -2,16 +2,13 @@ package imgShareAPI
 
 //package main
 
-/*TODO:
-implement functions
-structured error messages
-*/
-
 import (
 	"context"
 	"io"
 	"log"
 	"net/http"
+	"slices"
+	"strings"
 
 	"cloud.google.com/go/storage"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
@@ -26,10 +23,20 @@ type StorageUser struct {
 	filePath     string
 }
 
+type ImageListing struct {
+	Name string `json:"name"`
+	Link string `json:"link"`
+}
+
 const (
 	projectName = "img-share-api-project"
 	bucketName  = "img-share-api-func-bucket"
 )
+
+var validFileTypes = []string{".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png",
+	".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw", ".k25", ".bmp", ".dib",
+	".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpx", ".jpm", ".mj2", ".svg",
+	".svgz", ".ai", ".eps", ".pdf"}
 
 var user *StorageUser
 
@@ -66,14 +73,22 @@ func imgShareAPIFunc(c *gin.Context) {
 	// provide an api guide here?
 	// 	could use an html thing
 	//	https://gin-gonic.com/docs/examples/html-rendering/
-	c.String(http.StatusOK, "img-share-api")
+	c.String(http.StatusOK, "img-share-api\nFile upload POST requests should be made with \"file\" as form name")
 }
 
 func getAllImages(c *gin.Context) {
 	query := &storage.Query{Prefix: user.filePath}
 
-	var imageNames []string
+	var imageListings []ImageListing
+	//var imageNames []string
 	it := user.bucketHandle.Objects(*user.context, query)
+
+	//skip the foldername
+	_, err := it.Next()
+	if err == iterator.Done {
+		//c.IndentedJSON(http.StatusOK, imageNames)
+		c.IndentedJSON(http.StatusOK, imageListings)
+	}
 	for {
 		attrs, err := it.Next()
 		if err == iterator.Done {
@@ -81,13 +96,15 @@ func getAllImages(c *gin.Context) {
 		}
 		if err != nil {
 			log.Fatalf("Failed to iterate through objects in bucket: %v", err)
+			return
 		}
-
-		imageNames = append(imageNames, attrs.Name)
-		imageNames = append(imageNames, attrs.MediaLink)
+		imageListings = append(imageListings, ImageListing{attrs.Name, "https://storage.googleapis.com/" + bucketName + "/" + attrs.Name})
+		// imageNames = append(imageNames, attrs.Name)
+		// imageNames = append(imageNames, "https://storage.googleapis.com/"+bucketName+"/"+attrs.Name)
 	}
 
-	c.IndentedJSON(http.StatusOK, imageNames)
+	//c.IndentedJSON(http.StatusOK, imageNames)
+	c.IndentedJSON(http.StatusOK, imageListings)
 }
 
 func getSpecificImage(c *gin.Context) { //functions more like a search
@@ -102,6 +119,7 @@ func getSpecificImage(c *gin.Context) { //functions more like a search
 		}
 		if err != nil {
 			log.Fatalf("Failed to iterate through objects in bucket: %v", err)
+			return
 		}
 
 		imageNames = append(imageNames, attrs.Name)
@@ -112,18 +130,50 @@ func getSpecificImage(c *gin.Context) { //functions more like a search
 }
 
 func uploadImage(c *gin.Context) {
-	file, err := c.FormFile("file_input")
+	file, err := c.FormFile("file")
 	if err != nil {
-		log.Fatalf("Failed to receive file from request.")
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
 		return
 	}
-	blob, _ := file.Open()
 
-	//add timeout?
+	extensionIndex := strings.LastIndex(file.Filename, ".")
+	if len(file.Filename) == 0 || extensionIndex >= len(file.Filename) || extensionIndex == -1 {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Incorrect filetype or file name. Please use an image file of approved type",
+			"part2": "length check",
+		})
+		return
+	}
+	extension := file.Filename[strings.LastIndex(file.Filename, "."):len(file.Filename)]
+	if !slices.Contains(validFileTypes, extension) {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error":     "Incorrect filetype or file name. Please use an image file of approved type",
+			"part2":     "filename check",
+			"extension": extension,
+		})
+		return
+	}
+
+	blob, err := file.Open()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	//ADD TIMEOUT FEATURE HERE
+	//error checking + messages for each of these
+	//assign ID + other appropriate metadata
 	wc := *user.userClient.Bucket(bucketName).Object(user.filePath + file.Filename).NewWriter(*user.context)
 	io.Copy(&wc, blob)
 	wc.Close()
-	c.String(http.StatusOK, "upload image")
+	c.JSON(http.StatusOK, gin.H{
+		//"message": "upload successful",
+		"message": file.Filename + " uploaded successfully",
+	})
 }
 
 func deleteImage(c *gin.Context) {
