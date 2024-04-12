786654d8c4640d533864f10f56cfefc78d20a921
anthonyjdelpino <anthonyjdelpino@gmail.com>
2024-04-12 17:16:26 -0400

    Added image deletion and image id's derived from GCS object MD5 hash corresponding to image

--- index.go
+++ index.go
@@ -4,11 +4,13 @@ package imgShareAPI
 
 import (
 	"context"
+	"encoding/hex"
 	"io"
 	"log"
 	"net/http"
 	"slices"
 	"strings"
+	"time"
 
 	"cloud.google.com/go/storage"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
@@ -17,7 +19,7 @@ import (
 )
 
 type StorageUser struct {
-	userClient   *storage.Client
+	client       *storage.Client
 	context      *context.Context
 	bucketHandle *storage.BucketHandle
 	filePath     string
@@ -26,6 +28,7 @@ type StorageUser struct {
 type ImageListing struct {
 	Name string `json:"name"`
 	Link string `json:"link"`
+	ID   string `json:"id"`
 }
 
 const (
@@ -33,6 +36,7 @@ const (
 	bucketName  = "img-share-api-func-bucket"
 )
 
+// All acceptable filetypes for upload
 var validFileTypes = []string{".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png",
 	".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw", ".k25", ".bmp", ".dib",
 	".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpx", ".jpm", ".mj2", ".svg",
@@ -42,15 +46,18 @@ var user *StorageUser
 
 func init() {
 	//func main() {
+
+	//start Gin engine and set routes from API points to handler functions
 	route := gin.Default()
 	route.GET("/", imgShareAPIFunc)
 	route.GET("/images", getAllImages)
-	route.GET("/images/:name", getSpecificImage)
+	route.GET("/images/:id", getSpecificImage)
 	route.POST("/images", uploadImage)
 	route.DELETE("/images/:id", deleteImage)
 
 	functions.HTTP("imgShareAPIFunc", route.Handler().ServeHTTP)
 
+	//Create Google Cloud Platform API client
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx)
 	if err != nil {
@@ -58,9 +65,10 @@ func init() {
 	}
 	defer client.Close()
 
+	//Populate user struct with information for later use and set bucket
 	bucket := client.Bucket(bucketName)
 	user = &StorageUser{
-		userClient:   client,
+		client:       client,
 		context:      &ctx,
 		bucketHandle: bucket,
 		filePath:     "images/",
@@ -69,26 +77,28 @@ func init() {
 	//route.Run("localhost:8080")
 }
 
+// Here for the sake of Google Cloud Functions endpoint
 func imgShareAPIFunc(c *gin.Context) {
-	// provide an api guide here?
-	// 	could use an html thing
-	//	https://gin-gonic.com/docs/examples/html-rendering/
 	c.String(http.StatusOK, "img-share-api\nFile upload POST requests should be made with \"file\" as form name")
 }
 
+// Return a list of all images in the folder with a media link
 func getAllImages(c *gin.Context) {
 	query := &storage.Query{Prefix: user.filePath}
 
 	var imageListings []ImageListing
-	//var imageNames []string
 	it := user.bucketHandle.Objects(*user.context, query)
 
-	//skip the foldername
+	//skip the first item as it will just be the folder name
 	_, err := it.Next()
 	if err == iterator.Done {
-		//c.IndentedJSON(http.StatusOK, imageNames)
-		c.IndentedJSON(http.StatusOK, imageListings)
+		c.IndentedJSON(http.StatusOK, gin.H{
+			user.filePath: imageListings,
+		})
 	}
+
+	//Iterate through each item in the name path provided
+	//	-note that Cloud Storage uses a flat namespace: https://cloud.google.com/storage/docs/objects
 	for {
 		attrs, err := it.Next()
 		if err == iterator.Done {
@@ -98,19 +108,18 @@ func getAllImages(c *gin.Context) {
 			log.Fatalf("Failed to iterate through objects in bucket: %v", err)
 			return
 		}
-		imageListings = append(imageListings, ImageListing{attrs.Name, "https://storage.googleapis.com/" + bucketName + "/" + attrs.Name})
-		// imageNames = append(imageNames, attrs.Name)
-		// imageNames = append(imageNames, "https://storage.googleapis.com/"+bucketName+"/"+attrs.Name)
+		imageListings = append(imageListings, ImageListing{attrs.Name, "https://storage.googleapis.com/" + bucketName + "/" + attrs.Name, hex.EncodeToString(attrs.MD5)})
 	}
 
-	//c.IndentedJSON(http.StatusOK, imageNames)
 	c.IndentedJSON(http.StatusOK, imageListings)
 }
 
-func getSpecificImage(c *gin.Context) { //functions more like a search
-	query := &storage.Query{Prefix: user.filePath + c.Param("name")}
+// Return an image's information as identified by it's ID field
+//
+//	-ID is derived by hex-encoding the image's Cloud Storage object's MD5 hash
+func getSpecificImage(c *gin.Context) {
+	query := &storage.Query{Prefix: user.filePath}
 
-	var imageNames []string
 	it := user.bucketHandle.Objects(*user.context, query)
 	for {
 		attrs, err := it.Next()
@@ -121,15 +130,20 @@ func getSpecificImage(c *gin.Context) { //functions more like a search
 			log.Fatalf("Failed to iterate through objects in bucket: %v", err)
 			return
 		}
-
-		imageNames = append(imageNames, attrs.Name)
-		imageNames = append(imageNames, attrs.MediaLink)
+		if hex.EncodeToString(attrs.MD5) == c.Param("id") {
+			c.JSON(http.StatusOK, ImageListing{attrs.Name, "https://storage.googleapis.com/" + bucketName + "/" + attrs.Name, hex.EncodeToString(attrs.MD5)})
+			return
+		}
 	}
 
-	c.IndentedJSON(http.StatusOK, imageNames)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "No image found with ID: " + c.Param("id"),
+	})
 }
 
+// Upload images to the bucket in the folder specified in user.filePath
 func uploadImage(c *gin.Context) {
+	//Get multipart file from the POST request
 	file, err := c.FormFile("file")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -138,20 +152,18 @@ func uploadImage(c *gin.Context) {
 		return
 	}
 
+	//Make sure the file name uses an acceptable filetype extension
 	extensionIndex := strings.LastIndex(file.Filename, ".")
 	if len(file.Filename) == 0 || extensionIndex >= len(file.Filename) || extensionIndex == -1 {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": "Incorrect filetype or file name. Please use an image file of approved type",
-			"part2": "length check",
 		})
 		return
 	}
 	extension := file.Filename[strings.LastIndex(file.Filename, "."):len(file.Filename)]
 	if !slices.Contains(validFileTypes, extension) {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":     "Incorrect filetype or file name. Please use an image file of approved type",
-			"part2":     "filename check",
-			"extension": extension,
+			"error": "Incorrect filetype or file name. Please use an image file of approved type",
 		})
 		return
 	}
@@ -164,18 +176,69 @@ func uploadImage(c *gin.Context) {
 		return
 	}
 
-	//ADD TIMEOUT FEATURE HERE
-	//error checking + messages for each of these
-	//assign ID + other appropriate metadata
-	wc := *user.userClient.Bucket(bucketName).Object(user.filePath + file.Filename).NewWriter(*user.context)
+	_, cancel := context.WithTimeout(c, time.Second*10)
+	defer cancel()
+
+	//Create storage writer to write new object to bucket
+	wc := *user.client.Bucket(bucketName).Object(user.filePath + file.Filename).NewWriter(*user.context)
 	io.Copy(&wc, blob)
 	wc.Close()
+
 	c.JSON(http.StatusOK, gin.H{
-		//"message": "upload successful",
 		"message": file.Filename + " uploaded successfully",
 	})
 }
 
+// Delete image, identified in request by ID field
 func deleteImage(c *gin.Context) {
-	c.String(http.StatusOK, "delete image")
+	query := &storage.Query{Prefix: user.filePath}
+
+	var object *storage.ObjectHandle
+
+	it := user.bucketHandle.Objects(*user.context, query)
+	_, err := it.Next()
+	if err == iterator.Done {
+		c.JSON(http.StatusOK, gin.H{
+			"error": "Image not found. Folder is empty",
+		})
+	}
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate through objects in bucket: %v", err)
+			return
+		}
+		if hex.EncodeToString(attrs.MD5) == c.Param("id") {
+			object = user.client.Bucket(bucketName).Object(attrs.Name)
+		}
+		if object == nil {
+			//error here and return
+
+			log.Fatalf("Object nil: %v", attrs.Name)
+			return
+		}
+		attrs, err = object.Attrs(c)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Object does not exist")
+			return
+		}
+
+		object = object.If(storage.Conditions{GenerationMatch: attrs.Generation})
+		name := object.ObjectName()
+
+		_, cancel := context.WithTimeout(c, time.Second*10)
+		defer cancel()
+		if err := object.Delete(c); err != nil {
+			log.Fatalf("Failed to delete object: %v", err)
+			return
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"message": name + "was deleted",
+			})
+			return
+		}
+	}
 }
